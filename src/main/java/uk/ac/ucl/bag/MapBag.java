e9e61f9d59bc6f95bcb9ce5f6843370a9d92a7fd
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-02-25 01:53:35 +0000

    v3

--- src/main/java/uk/ac/ucl/bag/MapBag.java
+++ src/main/java/uk/ac/ucl/bag/MapBag.java
@@ -1,5 +1,34 @@
 package uk.ac.ucl.bag;
 
-public class MapBag<T extends Comparable> extends AbstractBag<T>{
+import java.util.HashMap;
+import java.util.Iterator;
 
+public class MapBag<T extends Comparable> extends AbstractBag<T>{
+    public void add(T value) throws bagException{
+    }
+
+    public void addWithOccurrences(T value, int occurrences) throws BagException {
+    }
+
+    public boolean contains(T value) {
+    }
+
+    public int countOf(T value) {
+    }
+
+    public void remove(T value) {
+    }
+
+    public int size() {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public Iterator<T> allOccurrencesIterator() {
+    }
+
+    public Iterator<T> iterator() {
+    }
 }
+
