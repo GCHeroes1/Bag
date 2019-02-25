8dd03d2b2023f30f8c9eb275cae67593778e21e9
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-02-25 04:14:43 +0000

    v5

--- src/main/java/uk/ac/ucl/bag/MapBag.java
+++ src/main/java/uk/ac/ucl/bag/MapBag.java
@@ -3,8 +3,62 @@ package uk.ac.ucl.bag;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class MapBag<T extends Comparable> extends AbstractBag<T>{
-    public void add(T value) throws bagException{
+public class MapBag<T extends Comparable> extends AbstractBag<T> {
+
+    private static class Entry<K extends Comparable, V extends Comparable>
+    {
+        public final K key;           //Act as the key for the hashMap
+        public V value;            //Acts the the value
+        public Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next)
+        {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private int maxSize;
+    private Entry<K, V> contents;
+
+    public MapBag() throws BagException
+    {
+        this(MAX_SIZE);
+    }
+
+    public MapBag(int maxSize) throws BagException
+    {
+        if (maxSize > MAX_SIZE)
+        {
+            throw new BagException("Attempting to create a Bag with size greater than maximum");
+        }
+        if (maxSize < 1)
+        {
+            throw new BagException("Attempting to create a Bag with size less than 1");
+        }
+        this.maxSize = maxSize;
+        this.contents = new HashMap<Entry>();
+    }
+
+    public void add(T value) throws BagException
+    {
+        for (ArrayBag.Element element : contents)
+        {
+            if (element.value.compareTo(value) == 0) // Must use compareTo to compare values.
+            {
+                element.count++;
+                return;
+            }
+        }
+        if (contents.size() < maxSize)
+        {
+            contents.add(new ArrayBag.Element(1,value));
+        }
+        else
+        {
+            throw new BagException("Bag is full");
+        }
     }
 
     public void addWithOccurrences(T value, int occurrences) throws BagException {
@@ -23,6 +77,9 @@ public class MapBag<T extends Comparable> extends AbstractBag<T>{
     }
 
     public boolean isEmpty() {
+    }
+
+    private class MapBagUniqueIterator implements Iterator<T> {
     }
 
     public Iterator<T> allOccurrencesIterator() {
