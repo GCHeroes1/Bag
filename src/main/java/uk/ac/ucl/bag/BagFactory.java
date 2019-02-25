29385fbc3e6fa746f95a13123d72f1f49cef2ebe
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-02-25 21:05:11 +0000

    v12

--- src/main/java/uk/ac/ucl/bag/BagFactory.java
+++ src/main/java/uk/ac/ucl/bag/BagFactory.java
@@ -58,9 +58,9 @@ public class BagFactory<T extends Comparable>
    */
   public Bag<T> getBag(int maxSize) throws BagException
   {
-    if (bagClass.equals("ArrayBag"))
+    if (bagClass.equals("MapBag"))
     {
-      return new ArrayBag<T>(maxSize);
+      return new MapBag<T>(maxSize);
     }
     throw new BagException
       ("Attempting to use BagFactory to create something that is not a Bag");
---
+++ src/test/java/uk/ac/ucl/MapBagTest.java
@@ -0,0 +1,25 @@
+package uk.ac.ucl;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ucl.bag.Bag;
+/*import uk.ac.ucl.bag.BagFactory;
+import uk.ac.ucl.bag.MapBag;
+
+public class MapBagTest {
+    private BagFactory<String> bagFactory = BagFactory.getInstance();
+    //static BagFactory bagFactory;
+    @Before
+    public void setUp() {
+        bagFactory = BagFactory.getBag();
+        bagFactory.setBagClass("MapBag");
+        Bag<String> bag1;
+        bag1 = bagFactory.getBag();
+        bag1.add("abc");
+        bag1.add("def");
+        bag1.add("hij");
+
+
+    }
+}*/
