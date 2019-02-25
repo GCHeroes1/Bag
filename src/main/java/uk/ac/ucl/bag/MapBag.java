ffea7a2b21eec40a885b56b5cf3c29c701deff30
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-02-25 04:38:38 +0000

    v6

--- src/main/java/uk/ac/ucl/bag/MapBag.java
+++ src/main/java/uk/ac/ucl/bag/MapBag.java
@@ -5,61 +5,34 @@ import java.util.Iterator;
 
 public class MapBag<T extends Comparable> extends AbstractBag<T> {
 
-    private static class Entry<K extends Comparable, V extends Comparable>
-    {
-        public final K key;           //Act as the key for the hashMap
-        public V value;            //Acts the the value
-        public Entry<K, V> next;
-
-        public Entry(K key, V value, Entry<K, V> next)
-        {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     private int maxSize;
-    private Entry<K, V> contents;
+    private HashMap<T, Integer> contents;
 
-    public MapBag() throws BagException
-    {
+    public MapBag() throws BagException {
         this(MAX_SIZE);
     }
 
-    public MapBag(int maxSize) throws BagException
-    {
-        if (maxSize > MAX_SIZE)
-        {
+    public MapBag(int maxSize) throws BagException {
+        if (maxSize > MAX_SIZE) {
             throw new BagException("Attempting to create a Bag with size greater than maximum");
         }
-        if (maxSize < 1)
-        {
+        if (maxSize < 1) {
             throw new BagException("Attempting to create a Bag with size less than 1");
         }
         this.maxSize = maxSize;
-        this.contents = new HashMap<Entry>();
+        this.contents = new HashMap<>();
     }
 
-    public void add(T value) throws BagException
-    {
-        for (ArrayBag.Element element : contents)
-        {
-            if (element.value.compareTo(value) == 0) // Must use compareTo to compare values.
-            {
-                element.count++;
-                return;
-            }
-        }
-        if (contents.size() < maxSize)
-        {
-            contents.add(new ArrayBag.Element(1,value));
-        }
-        else
-        {
+    public void add(T value) throws BagException {
+        if (contents.containsKey(value)) {
+            contents.put(value, contents.get(value) + 1);
+        } else if (contents.size() < maxSize) {
+            contents.put(value, 1);
+        } else {
             throw new BagException("Bag is full");
         }
     }
+
 
     public void addWithOccurrences(T value, int occurrences) throws BagException {
     }
