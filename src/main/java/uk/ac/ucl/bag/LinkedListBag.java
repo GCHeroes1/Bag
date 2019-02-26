c2a8bb021353d84b5dbe691e3b09d7b38bc7e3d6
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-02-26 04:07:07 +0000

    v16

--- src/main/java/uk/ac/ucl/bag/LinkedListBag.java
+++ src/main/java/uk/ac/ucl/bag/LinkedListBag.java
@@ -23,6 +23,16 @@ public class LinkedListBag<T extends Comparable> extends AbstractBag<T> {
         this(MAX_SIZE);
     }
 
+    public int size() {
+        int counter = 0;
+        Element<T> current = this.contents;
+        while (current != null) {
+            counter++;
+            current = current.next;
+        }
+        return counter;
+    }
+
     public LinkedListBag(int maxSize) throws BagException{
         if (maxSize > MAX_SIZE) {
             throw new BagException("Attempting to create a Bag with size greater than maximum");
@@ -35,31 +45,60 @@ public class LinkedListBag<T extends Comparable> extends AbstractBag<T> {
     }
 
     private class LinkedListBagUniqueIterator implements Iterator<T>{
+        private int index = 0;
 
+        public boolean hasNext() {
+            if (index < size()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public T next(){
+            index++;
+            Element<T> current = contents;
+            current = current.next;
+            return (T) current;
+        }
     }
 
     public Iterator<T> iterator() {
         return new LinkedListBagUniqueIterator();
     }
 
-    private class LinkedListBagIterator implements Iterator<T>{
+    private class LinkedListBagIterator implements Iterator<T> {
+        /*private int index = 0;
+        private int count = 0;
+
+        public boolean hasNext()
+        {
+            if (index < size()) {
+                if (count < contents.get(index).count) return true;
+                if ((count == contents.get(index).count) && ((index + 1) < contents.size())) return true;
+            }
+            return false;
+        }
+
+        public T next(){}
+    }*/
 
     }
-
     public Iterator<T> allOccurrencesIterator() {
         return new LinkedListBagIterator();
     }
 
     @Override
     public void add(T value) throws BagException{
-        /*this.contents = new Element<T>((T)"snake",3,null);
+
+        this.contents = new Element<T>((T)"snake",3,null);
         this.contents.next = new Element<T>((T)"cat",2,null);
         this.contents.next.next = new Element<T>((T) "Lol", 2, null);
         this.current = this.contents;
         // Snake
         this.current =  this.current.next;
         // Cat
-        */
+
     }
 
     public void addWithOccurrences(T value, int occurrences) throws BagException{}
@@ -69,8 +108,6 @@ public class LinkedListBag<T extends Comparable> extends AbstractBag<T> {
     public int countOf(T value){}
 
     public void remove(T value){}
-
-    public int size(){}
 
     public boolean isEmpty(){}
 }
