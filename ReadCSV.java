f21a6dfa86c6918ee19eb2b7a117f8a5d6dfcef0
GCHeroes1 <45121027+GCHeroes1@users.noreply.github.com>
2019-03-05 16:37:58 +0000

    Add files via upload
    v2 - question 2 complete

--- ReadCSV.java
+++ ReadCSV.java
@@ -1,22 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCSV {
-    public void Scanner() throws FileNotFoundException {
+    public ArrayList<Patient> Scanner() throws FileNotFoundException {
         File myDirectory = new File("/home/rajesh/Documents/Programming Languages/Java/JavaCoursework2/src/COMP0004Data-master/patients100.csv");
         Scanner PatientCSV = new Scanner(new FileReader(myDirectory)).useDelimiter(",");
         ArrayList<Patient> patientArrayList = new ArrayList<>();
-        ArrayList<String> arrayList = new ArrayList<>();
-        String patient = PatientCSV.nextLine();
-        String[] patient2 = patient.split(",", -1);
-        patient = patient2.toString();
-        System.out.println(patient);
+        while (PatientCSV.hasNextLine()){
+            String patientHolder = PatientCSV.nextLine();
+            String[] patientStringArray = patientHolder.split(",", -1);
+            Patient patient = new Patient(patientStringArray[0], patientStringArray[1], patientStringArray[2], patientStringArray[3],
+                    patientStringArray[4], patientStringArray[5], patientStringArray[6], patientStringArray[7],
+                    patientStringArray[8], patientStringArray[9], patientStringArray[10], patientStringArray[11],
+                    patientStringArray[12], patientStringArray[13], patientStringArray[14], patientStringArray[15],
+                    patientStringArray[16], patientStringArray[17], patientStringArray[18], patientStringArray[19]);
+            patientArrayList.add(patient);
+        }
+        System.out.println(patientArrayList.get(105).getZip());
+        System.out.println(patientArrayList.size());
+        return patientArrayList;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        new ReadCSV().Scanner();
+    public static void main(String[] args){
+        try {
+            new ReadCSV().Scanner();
+        } catch (FileNotFoundException e) {
+            System.out.println("====> File not found...");
+        }
     }
 }
