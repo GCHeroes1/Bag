738d0f1632ddd93d499a7f11026f343fa120a1c8
GCHeroes1 <45121027+GCHeroes1@users.noreply.github.com>
2019-03-06 00:17:43 +0000

    Add files via upload
    v3 - q3 finished

---
+++ JSONFormatter.java
@@ -0,0 +1,123 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class JSONFormatter {
+    public ArrayList<StringBuilder> Formatter() throws FileNotFoundException {
+        ArrayList<StringBuilder> PatientList = new ArrayList<>();
+        ArrayList<Patient> PatientArrayList = ReadCSV.Scanner();
+        for (int i = 1; i < PatientArrayList.size(); i++) {
+            StringBuilder patient = new StringBuilder();
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getID());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getID());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getBirthdate());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getBirthdate());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getDeathdate());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getDeathdate());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getSSN());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getSSN());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getDrivers());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getDrivers());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getPassport());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getPassport());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getPrefix());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getPrefix());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getFirst());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getFirst());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getLast());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getLast());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getSuffix());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getSuffix());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getMaiden());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getMaiden());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getMartial());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getMartial());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getRace());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getRace());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getEthnicity());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getEthnicity());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getGender());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getGender());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getBirthplace());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getBirthplace());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getAddress());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getAddress());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getCity());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getCity());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getState());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getState());
+            patient.append("\" \n");
+            patient.append("\"");
+            patient.append(PatientArrayList.get(0).getZip());
+            patient.append("\": \"");
+            patient.append(PatientArrayList.get(i).getZip());
+            patient.append("\"");
+            PatientList.add(patient);
+        }
+        System.out.println(PatientList.get(5));
+        return PatientList;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new JSONFormatter().Formatter();
+        } catch (FileNotFoundException e) {
+            System.out.println("====> File not found...");
+        }
+    }
+}
--- ReadCSV.java
+++ ReadCSV.java
@@ -1,12 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCSV {
-    public ArrayList<Patient> Scanner() throws FileNotFoundException {
+    public static ArrayList<Patient> Scanner() throws FileNotFoundException {
         File myDirectory = new File("/home/rajesh/Documents/Programming Languages/Java/JavaCoursework2/src/COMP0004Data-master/patients100.csv");
         Scanner PatientCSV = new Scanner(new FileReader(myDirectory)).useDelimiter(",");
         ArrayList<Patient> patientArrayList = new ArrayList<>();
@@ -19,9 +18,8 @@ public class ReadCSV {
                     patientStringArray[12], patientStringArray[13], patientStringArray[14], patientStringArray[15],
                     patientStringArray[16], patientStringArray[17], patientStringArray[18], patientStringArray[19]);
             patientArrayList.add(patient);
+
         }
-        System.out.println(patientArrayList.get(105).getZip());
-        System.out.println(patientArrayList.size());
         return patientArrayList;
     }
 
